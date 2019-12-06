5b4dab9c0a40a58bd27193cb37dc57f6e41da0bb
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-12-05 20:02:09 -0800

    Finished SongDetail with conditional rendering.

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -2,8 +2,19 @@ import React from "react";
 import { connect } from "react-redux";
 
 const SongDetail = props => {
-  console.log(props);
-  return <div>SongDetail</div>;
+  if (!props.song) {
+    return <div>Select a song...</div>;
+  }
+  return (
+    <div>
+      <h3>Details for:</h3>
+      <p>
+        Title: {props.song.title}
+        <br />
+        Duration: {props.song.time}
+      </p>
+    </div>
+  );
 };
 
 const mapStateToProps = state => {
